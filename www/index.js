f9bc3c30b26310e7269bfce969381a5cfa844834
tak001 <twitwitwi2@yahoo.co.jp>
2021-06-20 11:23:11 +0900

    wasm-game-of-life

---
+++ www/index.js
@@ -0,0 +1,13 @@
+import { Universe } from "wasm-game-of-life";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
